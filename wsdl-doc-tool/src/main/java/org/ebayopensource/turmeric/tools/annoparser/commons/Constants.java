d5c14e702ec25de4a6ccd9d55fa86485765855b1
Saikiran Daripelli <sdaripelli@ebay.com>
2011-07-07 14:24:36 +0530

    TURMERIC-1480 Build failed, due to some non-UTF8 character added by eclipse.
    Change-Id: Ieb4199f6fcb00b4a69c6cab128879a7781788ab8

--- wsdl-doc-tool/src/main/java/org/ebayopensource/turmeric/tools/annoparser/commons/Constants.java
+++ wsdl-doc-tool/src/main/java/org/ebayopensource/turmeric/tools/annoparser/commons/Constants.java
@@ -8,229 +8,362 @@
  *******************************************************************************/
 package org.ebayopensource.turmeric.tools.annoparser.commons;
 
-
 /**
  * The Interface Constants.
- *
+ * 
  * @author goraman
  */
 public interface Constants {
 
-	 String DOT_HTML = ".html";
-	
-	 String XSD_COMPLEX_TYPE = "xsd:complexType";
-	
-	 String XS_COMPLEX_TYPE = "xs:complexType";
-	
-	 String XSD_SIMPLE_TYPE = "xs:simpleType";
-	
-	 String XS_SIMPLE_TYPE = "xs:simpleType";
-	
-	 String XSD_ENUMERATION = "xsd:enumeration";
-	
-	 String XS_ENUMERATION = "xs:enumeration";
-	
-	 String XSD_ELEMENT = "xsd:element";
-	
-	 String XS_ELEMENT = "xs:element";
-	
-	 String XSD_DOCUMENTATION = "xs:documentation";
-	
-	 String XS_DOCUMENTATION = "xs:documentation";
+	/** The DO t_ html. */
+	String DOT_HTML = ".html";
 
-	 String SUPPORT = "Support";
-	 
-	 String CLASSES = "Services";
-	 
-	 String CLASSFRAME = "classframe";
-	 
-	 String ALL_CLASSES = "All Services";
-	 
-	 String ALLCLASSES = "allclasses";
-	 
-	 String PACKAGES = "Service Domains";
-	 
-	 String PACKAGES_INDEX = "packagesindex";
-	 
-	 String CLASSESFRAME = "classesframe";
-	 
-	 String INDEX = "index";
-	 
-	 String FIELDS = "Fields";
-	
-	 String SUPPORT_URL = "http://developer.ebay.com/support/";
-	
-	 String KNOWLEDGEB_BASE = "Knowledge Base";
-	
-	 String KNOWLEDGEB_BASE_URL = "http://developer.ebay.com/DevZone/support/knowledgebase/";
-	
-	 String FORUMS = "Forums";
-	
-	 String FORUMS_URL = "http://developer.ebay.com/community/forums/";
-	
-	 String ALL_TUTORIALS = "All Tutorials";
-	
-	 String ALL_TUTORIALS_URL = "http://developer.ebay.com/support/docs/tutorials/";
-	
-	 String ALL_CODE_SAMPLES = "All Code Samples";	
-	
-	 String ALL_CODE_SAMPLES_URL = "http://developer.ebay.com/community/featured_projects/?name=Code+Samples+Project";
-	
-	 String API_REFERENCE_DOCS = "apireferencedocs";
-	
-	 String API_REFERENCE_DOCS_URL = "http://developer.ebay.com/community/codebase/apireferencedocs/default.aspx"; 
-	
-	 String SERVICE_NAME = "<font id='serviceName'>{#} API</font>";
-	
-	 String CALL_REF_VERSION = "<font id='callRefVersion'>Call Reference Version {#}</font>";
-	
-	 String COPYRIGHTS = "© 2010 eBay, Inc. All rights reserved. This documentation and the API may only be used in accordance with the eBay Developers Program and API License Agreement.";
-	
-	 String DOC_TOOL_FOOTER_INFO = "This document was generated with a customized version of the {#} tool.";
-	
-	 String ADDITIONAL_DESCRIPTION = "Additional resources are available for this eBay Web Service. Please see the";
-	
-	 String ADDITIONAL_DESCRIPTION_URL_NAME = "eBay Developer Documentation Center";
-	
-	 String ADDITIONAL_DESCRIPTION_URL_LINK = "http://developer.ebay.com/support/docs/";	
-	
-	 String TABLE_OF_CONTENTS = "Table of Contents";
-	
-	 String TYPE_INDEX_FILE_NAME = "index";
-	
-	 String TYPE_INDEX = "Type Index";
-	
-	 String TYPE_INDEX_DESCRIPTION = "This is a list of all types in the schema, types upon which fields are based. You can learn more about how different calls and different types make use of these types.";
-	
-	 String TYPE_INDEX_TITLE = "All types upon which schema fields are based";
-	
-	 String FIELD_INDEX_FILE_NAME = "fieldindex";
-	
-	 String FIELD_INDEX = "Field Index";
-	
-	 String FIELD_INDEX_DESCRIPTION = "This is a list of all fields in the schema, by name. The same field name may be used by several calls. Note that the underlying type of the field may or may not be the same.";
-	
-	 String FIELD_INDEX_TITLE = "All fields in the schema";
-	
-	 String FIELD_FIELD_INDEX_FILE_NAME = "fieldfieldindex";
-	
-	 String FIELD_FIELD_INDEX = "Field Field Index";
-	
-	 String FIELD_FIELD_INDEX_DESCRIPTION = "This is a list of all unique field names in the schema";
-	
-	 String FIELD_FIELD_INDEX_DESCRIPTION_2 = "The benefit of this page over the regular Field Index is that this page is good for searches with Ctrl-F, for example, to help you find each field whose name includes the word Feedback. Note that each field name in this page is a link to the corresponding name in the Field Index (so that you can see in what ways each field name is used).";
-	
-	 String ENUMERATION_INDEX_FILE_NAME = "enumindex";
-	
-	 String ENUMERATION_INDEX = "Enumeration Index";
-	
-	 String ENUMERATION_INDEX_DESCRIPTION = "This is a list of all enumeration values in the schema. The same enumeration value name may be used by several calls.";
-	
-	 String ENUMERATION_INDEX_TITLE = "All enumeration values in the schema";
+	/** The XS d_ comple x_ type. */
+	String XSD_COMPLEX_TYPE = "xsd:complexType";
 
-	 String SIMPLE_TYPE_NOTE = " * See the {#} to see exact use of each enumeration value in the API. ";
-	
-	 String LIST_OF_ALL_CALLS_INDEX_FILE_NAME = "index";
-	
-	 String LIST_OF_ALL_CALLS_INDEX = "List of All Calls";
-	
-	 String LIST_OF_ALL_CALLS_INDEX_DESCRIPTION = "This Call Reference describes the elements and attributes for each call in the BestMatchItemDetailsInternal API.";
-	
-	 String LIST_OF_ALL_CALLS_INDEX_TITLE = "List of all available calls in this schema";
-	
-	 String CALL_INPUT_TABLE_DESCRIPTION = "The box below lists all fields that could be included in the call request. To learn more about an individual field or its type, click its name in the box (or scroll down to find it in the table below the box).";
-	 
-	 String CALL_OUTPUT_TABLE_DESCRIPTION = "The box below lists all fields that might be returned in the response. To learn more about an individual field or its type, click its name in the box (or scroll down to find it in the table below the box).";
-	
+	/** The X s_ comple x_ type. */
+	String XS_COMPLEX_TYPE = "xs:complexType";
 
-	 String HTML_BR = "<br/>";
-	 String HTML_BR_TWICE=HTML_BR+HTML_BR;
-	 String HTML_HR = "<hr/>";
+	/** The XS d_ simpl e_ type. */
+	String XSD_SIMPLE_TYPE = "xs:simpleType";
 
-	 String HTML_TABLE_START_WITH_ATTRS = "<table {#}>";
-	 String HTML_TABLE_END = "</table>";
-	 String HTML_TABLE_TR_START_WITH_ATTRS = "<tr {#}>";
-	 String HTML_TABLE_TR_END = "</tr>";
-	 String HTML_TABLE_TH_START_WITH_ATTRS = "<th {#}>";
-	 String HTML_TABLE_TH_END = "</th>";
-	 String HTML_TABLE_TD_START_WITH_ATTRS = "<td {#}>";
-	 String HTML_TABLE_TD_END = "</td>";
-	 String HTML_DIV_START = "<div {#}>";
-	 String HTML_DIV_END = "</div>";
-	 String HTML_SPAN_START = "<span {#}>";
-	 String HTML_SPAN_END = "</span>";
-	 String HTML_P_START = "<p>";	 
-	 String HTML_P_END = "</p>";
-	 
-	 String COMPLEX_TYPE_HEADER = "ComplexTypeHeader";
-	 String SIMPLE_TYPE_HEADER = "SimpleTypeHeader";
-	 String OPERATION_HEADER = "operationHeader";
-	 String TREE_HEADER = "TreeHeader";
-	 String USE_HEADER = "UseHeader";
-	 
-	 String NBSP = "&nbsp;";
-	 
-	 String NBSP_TWICE = "&nbsp;&nbsp;";
-	 
-	 String NBSP_THRICE = "&nbsp;&nbsp;&nbsp;";
-	 
-	 String INPUT_DOCUMENT="documents";
-	
-	 String INPUT_OUTPUT_DIR="output_dir";
-	
-	 String INPUT_CONFIG="config";
-	
-	 String INPUT_CSS="css";	
-	
-	 String INPUT_SYSENV_OUTPUT_DIR="ANNOPARSER_OUTPUT_DIR";
-	
-	 String INPUT_SYSENV_CONFIG="ANNOPARSER_CONFIG";
-	
-	 String INPUT_SYSENV_CSS="ANNOPARSER_CSS";	
-	
-	String OPERATION_SUMMARY_HREF="OperationSummary";
-	
-	String OPERATION_SUMMARY_LABEL="Operation Summary";
-	
-	String OPERATIONS_LABEL="Operations";
-	String OPERATIONS_DETAIL_HREF="OperationDetail";
-	String OPERATION_DETAIL_LABEL="Operation Detail";
-	
-	String HTML_H2_START="<h2>";
-	
-	String HTML_H3_START="<h3>";
-	
-	String HTML_H3_END="</h3>";
-	
-	String SERVICE_LABEL="Service: ";
-	
-	String HTML_H2_END="</h2>";
-	
-	String HTML_DL_START="<dl>";
-	
-	String HTML_DD_START="<dd class=\"JavadocDocumentationTxt\">";
-	
-	String HTML_DL_END="</dl>";
-	
-	String HTML_DD_END="</dd>";
-	
-	String RETURN_TYPE_LABEL="Return Type";
-	
-	String METHOD_NAME_LABEL="Method Name";
-	
-	String HTML_PRE_START="<pre>";
-	
-	String HTML_PRE_END="</pre>";
-	
-    String HTML_BOLD_START="<b>";
-	
-	String HTML_BOLD_END="</b>";
-	
-	
+	/** The X s_ simpl e_ type. */
+	String XS_SIMPLE_TYPE = "xs:simpleType";
 
-	
-	String COMMAND_LINE_HELP_ARG="help";
-	
-	 String CSS_STYLE_SUB_HEADING = "subHeading";
-	
+	/** The XS d_ enumeration. */
+	String XSD_ENUMERATION = "xsd:enumeration";
+
+	/** The X s_ enumeration. */
+	String XS_ENUMERATION = "xs:enumeration";
+
+	/** The XS d_ element. */
+	String XSD_ELEMENT = "xsd:element";
+
+	/** The X s_ element. */
+	String XS_ELEMENT = "xs:element";
+
+	/** The XS d_ documentation. */
+	String XSD_DOCUMENTATION = "xs:documentation";
+
+	/** The X s_ documentation. */
+	String XS_DOCUMENTATION = "xs:documentation";
+
+	/** The SUPPORT. */
+	String SUPPORT = "Support";
+
+	/** The CLASSES. */
+	String CLASSES = "Services";
+
+	/** The CLASSFRAME. */
+	String CLASSFRAME = "classframe";
+
+	/** The AL l_ classes. */
+	String ALL_CLASSES = "All Services";
+
+	/** The ALLCLASSES. */
+	String ALLCLASSES = "allclasses";
+
+	/** The PACKAGES. */
+	String PACKAGES = "Service Domains";
+
+	/** The PACKAGE s_ index. */
+	String PACKAGES_INDEX = "packagesindex";
+
+	/** The CLASSESFRAME. */
+	String CLASSESFRAME = "classesframe";
+
+	/** The INDEX. */
+	String INDEX = "index";
+
+	/** The FIELDS. */
+	String FIELDS = "Fields";
+
+	/** The SUPPOR t_ url. */
+	String SUPPORT_URL = "http://developer.ebay.com/support/";
+
+	/** The KNOWLEDGE b_ base. */
+	String KNOWLEDGEB_BASE = "Knowledge Base";
+
+	/** The KNOWLEDGE b_ bas e_ url. */
+	String KNOWLEDGEB_BASE_URL = "http://developer.ebay.com/DevZone/support/knowledgebase/";
+
+	/** The FORUMS. */
+	String FORUMS = "Forums";
+
+	/** The FORUM s_ url. */
+	String FORUMS_URL = "http://developer.ebay.com/community/forums/";
+
+	/** The AL l_ tutorials. */
+	String ALL_TUTORIALS = "All Tutorials";
+
+	/** The AL l_ tutorial s_ url. */
+	String ALL_TUTORIALS_URL = "http://developer.ebay.com/support/docs/tutorials/";
+
+	/** The AL l_ cod e_ samples. */
+	String ALL_CODE_SAMPLES = "All Code Samples";
+
+	/** The AL l_ cod e_ sample s_ url. */
+	String ALL_CODE_SAMPLES_URL = "http://developer.ebay.com/community/featured_projects/?name=Code+Samples+Project";
+
+	/** The AP i_ referenc e_ docs. */
+	String API_REFERENCE_DOCS = "apireferencedocs";
+
+	/** The AP i_ referenc e_ doc s_ url. */
+	String API_REFERENCE_DOCS_URL = "http://developer.ebay.com/community/codebase/apireferencedocs/default.aspx";
+
+	/** The SERVIC e_ name. */
+	String SERVICE_NAME = "<font id='serviceName'>{#} API</font>";
+
+	/** The CAL l_ re f_ version. */
+	String CALL_REF_VERSION = "<font id='callRefVersion'>Call Reference Version {#}</font>";
+
+	/** The COPYRIGHTS. */
+	String COPYRIGHTS = "ï¿½ 2010 eBay, Inc. All rights reserved. This documentation and the API may only be used in accordance with the eBay Developers Program and API License Agreement.";
+
+	/** The DO c_ too l_ foote r_ info. */
+	String DOC_TOOL_FOOTER_INFO = "This document was generated with a customized version of the {#} tool.";
+
+	/** The ADDITIONA l_ description. */
+	String ADDITIONAL_DESCRIPTION = "Additional resources are available for this eBay Web Service. Please see the";
+
+	/** The ADDITIONA l_ descriptio n_ ur l_ name. */
+	String ADDITIONAL_DESCRIPTION_URL_NAME = "eBay Developer Documentation Center";
+
+	/** The ADDITIONA l_ descriptio n_ ur l_ link. */
+	String ADDITIONAL_DESCRIPTION_URL_LINK = "http://developer.ebay.com/support/docs/";
+
+	/** The TABL e_ o f_ contents. */
+	String TABLE_OF_CONTENTS = "Table of Contents";
+
+	/** The TYP e_ inde x_ fil e_ name. */
+	String TYPE_INDEX_FILE_NAME = "index";
+
+	/** The TYP e_ index. */
+	String TYPE_INDEX = "Type Index";
+
+	/** The TYP e_ inde x_ description. */
+	String TYPE_INDEX_DESCRIPTION = "This is a list of all types in the schema, types upon which fields are based. You can learn more about how different calls and different types make use of these types.";
+
+	/** The TYP e_ inde x_ title. */
+	String TYPE_INDEX_TITLE = "All types upon which schema fields are based";
+
+	/** The FIEL d_ inde x_ fil e_ name. */
+	String FIELD_INDEX_FILE_NAME = "fieldindex";
+
+	/** The FIEL d_ index. */
+	String FIELD_INDEX = "Field Index";
+
+	/** The FIEL d_ inde x_ description. */
+	String FIELD_INDEX_DESCRIPTION = "This is a list of all fields in the schema, by name. The same field name may be used by several calls. Note that the underlying type of the field may or may not be the same.";
+
+	/** The FIEL d_ inde x_ title. */
+	String FIELD_INDEX_TITLE = "All fields in the schema";
+
+	/** The FIEL d_ fiel d_ inde x_ fil e_ name. */
+	String FIELD_FIELD_INDEX_FILE_NAME = "fieldfieldindex";
+
+	/** The FIEL d_ fiel d_ index. */
+	String FIELD_FIELD_INDEX = "Field Field Index";
+
+	/** The FIEL d_ fiel d_ inde x_ description. */
+	String FIELD_FIELD_INDEX_DESCRIPTION = "This is a list of all unique field names in the schema";
+
+	/** The FIEL d_ fiel d_ inde x_ descriptio n_2. */
+	String FIELD_FIELD_INDEX_DESCRIPTION_2 = "The benefit of this page over the regular Field Index is that this page is good for searches with Ctrl-F, for example, to help you find each field whose name includes the word Feedback. Note that each field name in this page is a link to the corresponding name in the Field Index (so that you can see in what ways each field name is used).";
+
+	/** The ENUMERATIO n_ inde x_ fil e_ name. */
+	String ENUMERATION_INDEX_FILE_NAME = "enumindex";
+
+	/** The ENUMERATIO n_ index. */
+	String ENUMERATION_INDEX = "Enumeration Index";
+
+	/** The ENUMERATIO n_ inde x_ description. */
+	String ENUMERATION_INDEX_DESCRIPTION = "This is a list of all enumeration values in the schema. The same enumeration value name may be used by several calls.";
+
+	/** The ENUMERATIO n_ inde x_ title. */
+	String ENUMERATION_INDEX_TITLE = "All enumeration values in the schema";
+
+	/** The SIMPL e_ typ e_ note. */
+	String SIMPLE_TYPE_NOTE = " * See the {#} to see exact use of each enumeration value in the API. ";
+
+	/** The LIS t_ o f_ al l_ call s_ inde x_ fil e_ name. */
+	String LIST_OF_ALL_CALLS_INDEX_FILE_NAME = "index";
+
+	/** The LIS t_ o f_ al l_ call s_ index. */
+	String LIST_OF_ALL_CALLS_INDEX = "List of All Calls";
+
+	/** The LIS t_ o f_ al l_ call s_ inde x_ description. */
+	String LIST_OF_ALL_CALLS_INDEX_DESCRIPTION = "This Call Reference describes the elements and attributes for each call in the BestMatchItemDetailsInternal API.";
+
+	/** The LIS t_ o f_ al l_ call s_ inde x_ title. */
+	String LIST_OF_ALL_CALLS_INDEX_TITLE = "List of all available calls in this schema";
+
+	/** The CAL l_ inpu t_ tabl e_ description. */
+	String CALL_INPUT_TABLE_DESCRIPTION = "The box below lists all fields that could be included in the call request. To learn more about an individual field or its type, click its name in the box (or scroll down to find it in the table below the box).";
+
+	/** The CAL l_ outpu t_ tabl e_ description. */
+	String CALL_OUTPUT_TABLE_DESCRIPTION = "The box below lists all fields that might be returned in the response. To learn more about an individual field or its type, click its name in the box (or scroll down to find it in the table below the box).";
+
+	/** The HTM l_ br. */
+	String HTML_BR = "<br/>";
+
+	/** The HTM l_ b r_ twice. */
+	String HTML_BR_TWICE = HTML_BR + HTML_BR;
+
+	/** The HTM l_ hr. */
+	String HTML_HR = "<hr/>";
+
+	/** The HTM l_ tabl e_ star t_ wit h_ attrs. */
+	String HTML_TABLE_START_WITH_ATTRS = "<table {#}>";
+
+	/** The HTM l_ tabl e_ end. */
+	String HTML_TABLE_END = "</table>";
+
+	/** The HTM l_ tabl e_ t r_ star t_ wit h_ attrs. */
+	String HTML_TABLE_TR_START_WITH_ATTRS = "<tr {#}>";
+
+	/** The HTM l_ tabl e_ t r_ end. */
+	String HTML_TABLE_TR_END = "</tr>";
+
+	/** The HTM l_ tabl e_ t h_ star t_ wit h_ attrs. */
+	String HTML_TABLE_TH_START_WITH_ATTRS = "<th {#}>";
+
+	/** The HTM l_ tabl e_ t h_ end. */
+	String HTML_TABLE_TH_END = "</th>";
+
+	/** The HTM l_ tabl e_ t d_ star t_ wit h_ attrs. */
+	String HTML_TABLE_TD_START_WITH_ATTRS = "<td {#}>";
+
+	/** The HTM l_ tabl e_ t d_ end. */
+	String HTML_TABLE_TD_END = "</td>";
+
+	/** The HTM l_ di v_ start. */
+	String HTML_DIV_START = "<div {#}>";
+
+	/** The HTM l_ di v_ end. */
+	String HTML_DIV_END = "</div>";
+
+	/** The HTM l_ spa n_ start. */
+	String HTML_SPAN_START = "<span {#}>";
+
+	/** The HTM l_ spa n_ end. */
+	String HTML_SPAN_END = "</span>";
+
+	/** The HTM l_ p_ start. */
+	String HTML_P_START = "<p>";
+
+	/** The HTM l_ p_ end. */
+	String HTML_P_END = "</p>";
+
+	/** The COMPLE x_ typ e_ header. */
+	String COMPLEX_TYPE_HEADER = "ComplexTypeHeader";
+
+	/** The SIMPL e_ typ e_ header. */
+	String SIMPLE_TYPE_HEADER = "SimpleTypeHeader";
+
+	/** The OPERATIO n_ header. */
+	String OPERATION_HEADER = "operationHeader";
+
+	/** The TRE e_ header. */
+	String TREE_HEADER = "TreeHeader";
+
+	/** The US e_ header. */
+	String USE_HEADER = "UseHeader";
+
+	/** The NBSP. */
+	String NBSP = "&nbsp;";
+
+	/** The NBS p_ twice. */
+	String NBSP_TWICE = "&nbsp;&nbsp;";
+
+	/** The NBS p_ thrice. */
+	String NBSP_THRICE = "&nbsp;&nbsp;&nbsp;";
+
+	/** The INPU t_ document. */
+	String INPUT_DOCUMENT = "documents";
+
+	/** The INPU t_ outpu t_ dir. */
+	String INPUT_OUTPUT_DIR = "output_dir";
+
+	/** The INPU t_ config. */
+	String INPUT_CONFIG = "config";
+
+	/** The INPU t_ css. */
+	String INPUT_CSS = "css";
+
+	/** The INPU t_ sysen v_ outpu t_ dir. */
+	String INPUT_SYSENV_OUTPUT_DIR = "ANNOPARSER_OUTPUT_DIR";
+
+	/** The INPU t_ sysen v_ config. */
+	String INPUT_SYSENV_CONFIG = "ANNOPARSER_CONFIG";
+
+	/** The INPU t_ sysen v_ css. */
+	String INPUT_SYSENV_CSS = "ANNOPARSER_CSS";
+
+	/** The OPERATIO n_ summar y_ href. */
+	String OPERATION_SUMMARY_HREF = "OperationSummary";
+
+	/** The OPERATIO n_ summar y_ label. */
+	String OPERATION_SUMMARY_LABEL = "Operation Summary";
+
+	/** The OPERATION s_ label. */
+	String OPERATIONS_LABEL = "Operations";
+
+	/** The OPERATION s_ detai l_ href. */
+	String OPERATIONS_DETAIL_HREF = "OperationDetail";
+
+	/** The OPERATIO n_ detai l_ label. */
+	String OPERATION_DETAIL_LABEL = "Operation Detail";
+
+	/** The HTM l_ h2_ start. */
+	String HTML_H2_START = "<h2>";
+
+	/** The HTM l_ h3_ start. */
+	String HTML_H3_START = "<h3>";
+
+	/** The HTM l_ h3_ end. */
+	String HTML_H3_END = "</h3>";
+
+	/** The SERVIC e_ label. */
+	String SERVICE_LABEL = "Service: ";
+
+	/** The HTM l_ h2_ end. */
+	String HTML_H2_END = "</h2>";
+
+	/** The HTM l_ d l_ start. */
+	String HTML_DL_START = "<dl>";
+
+	/** The HTM l_ d d_ start. */
+	String HTML_DD_START = "<dd class=\"JavadocDocumentationTxt\">";
+
+	/** The HTM l_ d l_ end. */
+	String HTML_DL_END = "</dl>";
+
+	/** The HTM l_ d d_ end. */
+	String HTML_DD_END = "</dd>";
+
+	/** The RETUR n_ typ e_ label. */
+	String RETURN_TYPE_LABEL = "Return Type";
+
+	/** The METHO d_ nam e_ label. */
+	String METHOD_NAME_LABEL = "Method Name";
+
+	/** The HTM l_ pr e_ start. */
+	String HTML_PRE_START = "<pre>";
+
+	/** The HTM l_ pr e_ end. */
+	String HTML_PRE_END = "</pre>";
+
+	/** The HTM l_ bol d_ start. */
+	String HTML_BOLD_START = "<b>";
+
+	/** The HTM l_ bol d_ end. */
+	String HTML_BOLD_END = "</b>";
+
+	/** The COMMAN d_ lin e_ hel p_ arg. */
+	String COMMAND_LINE_HELP_ARG = "help";
+
+	/** The CS s_ styl e_ su b_ heading. */
+	String CSS_STYLE_SUB_HEADING = "subHeading";
+
 }
